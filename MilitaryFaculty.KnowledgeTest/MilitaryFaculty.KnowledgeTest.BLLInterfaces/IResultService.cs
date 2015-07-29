38c4427261f7b20d8fe7c0d3fb49bf93a830be6b
ArtsiomRusak <bembi1204@gmail.com>
2015-07-29 17:40:43 +0300

    Add item.

---
+++ MilitaryFaculty.KnowledgeTest/MilitaryFaculty.KnowledgeTest.BLLInterfaces/IResultService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MilitaryFaculty.KnowledgeTest.Entities.Entities;
+
+namespace MilitaryFaculty.KnowledgeTest.BLLInterfaces
+{
+    public interface IResultService
+    {
+        Result AddResult(Result result);
+        Result AddResult(int countOfRightAnswers, int countOfWrongAnswers, bool success, double mark, int studentId,
+            ResultConfig resultConfig);
+        IList<Result> GetResults();
+        IList<Result> GetResultsByStudentId(int studentId);
+    }
+}
